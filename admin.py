46360f7e80ec6daa9f120aace3a9782f72fab830
zhangwentao <taowenzhang@gmail.com>
2013-03-20 08:28:41 +0800

    加入 admin

---
+++ admin.py
@@ -0,0 +1,7 @@
+from models import Article,Article_Tag,Tag,Comment
+from django.contrib import admin
+
+admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Article_Tag)
+admin.site.register(Tag)
